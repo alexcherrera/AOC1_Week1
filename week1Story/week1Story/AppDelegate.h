de6017f25cbe1c96ca60aee89947d7c9a910c2c2
Alexander Herrera <alexcherrera@fullsail.edu>
2012-10-22 17:25:49 -0400

    Adding new folder created in Xcode

---
+++ week1Story/week1Story/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  week1Story
+//
+//  Created by Alexander Herrera on 10/22/12.
+//  Copyright (c) 2012 Alexander Herrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
