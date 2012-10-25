76331a7c70a46b125a09492739c6844c1abbcb2f
Alexander Herrera <alexcherrera@fullsail.edu>
2012-10-25 16:12:48 -0400

    Finished with my story and adding all the requirements

--- Week1/Week1/AppDelegate.m
+++ Week1/Week1/AppDelegate.m
@@ -3,6 +3,7 @@
 //  Week1
 //
 //  Created by Alexander Herrera on 10/24/12.
+//  MDVBS 10/12
 //  Copyright (c) 2012 Alexander Herrera. All rights reserved.
 //
 
@@ -27,10 +28,13 @@
 	//Story Variables
 	//Strings
 		NSString * myName = @"Alexander Herrera and I'm from Miami, Florida.";
-		NSString * myInfo = @"I'm in the Mobile Development Program.";
+		NSString * myInfo = @"Mobile Development Program.";
 	//Integers of the lengths regarding my studying and program time
 		int classLength = 1;
-		int academicYear = 8;	
+		int academicYear = 8;
+		int StartNewAcaYr = 1;
+		int currentMonth = 10;
+		int currentMonthInArr = currentMonth - 1;
 		int hrsMonDays = 10;
 		int hrsTuesDays = 12;
 		int hrsWedsDays = 14;
@@ -39,27 +43,91 @@
 		int hrsSatDays = 4;
 		float hrsSunDays = 5.00f;
 		int totalHrs = hrsMonDays + hrsTuesDays + hrsWedsDays + hrsThurDays + hrsFriDays + hrsSatDays + (int)hrsSunDays;
-	//Floats and using casting 
+	//Floats and using casting
+		int transferClass = 4;
 		int fullProgramLength = 32;
+		int myProgramLength = fullProgramLength - transferClass;
 		float averageHrs = (float)totalHrs / 7;
-		NSLog(@"%@",myName);
-		NSLog(@"%@",myInfo);
-		NSLog(@"%d",classLength);
-		NSLog(@"%d",academicYear);
-		NSLog(@"%d",hrsMonDays);
-		NSLog(@"%d",hrsTuesDays);
-		NSLog(@"%d",hrsWedsDays);
-		NSLog(@"%d",hrsThurDays);
-		NSLog(@"%d",hrsFriDays);
-		NSLog(@"%d",hrsSatDays);
-		NSLog(@"%f",hrsSunDays);
-		NSLog(@"%d",totalHrs);
-		NSLog(@"%d",fullProgramLength);
-		NSLog(@"%f",averageHrs);
-		BOOL buySeparate = true;
-		NSLog(@"%",buySeparate);
+		BOOL passedClass = true;
+		int hrsInAWeek = 168;
+		int hrsLeft = hrsInAWeek - totalHrs;
+		NSArray * eachCourse;
+		eachCourse = [NSArray arrayWithObjects: @"Apple Programming Language: Objective C I & Project Management and Assessment IV", @"Fundamentals of Physical Science", @"Apple Programming Language: Objective C II", @"Mobile Development Frameworks I", @"Mobile User Experience & Project Management and Assessment V", @"Mobile Development Frameworks II", @"Technical Writing", nil];
+		int count;
+		count = [eachCourse count];
 	//Story begins
-		
+		NSLog(@"My name is %@ and I'm in the %@", myName, myInfo);
+		NSLog(@"I started in January of this year and every class is %d month long and the whole program is %d months", classLength, fullProgramLength);
+		if (currentMonth > academicYear) {
+			NSLog(@"I'm in the Second academic year, which the classes are:");
+			for (int i = 0; i < count; i++) {
+				NSLog(@"%i %@", i+1,[eachCourse objectAtIndex:i]);
+			}
+		} else {
+			NSLog(@"I'm still in the First academic year.");
+		}
+		while (StartNewAcaYr < academicYear) {
+			int result = academicYear - StartNewAcaYr;
+			NSLog(@"I'm still in the second academic year but I %d months left", result);
+			StartNewAcaYr++;
+		}
+		NSLog(@"Ever since I started this program I been spending hours everyday studying,");
+		//Mondays Conditionals
+		if (hrsMonDays < 10) {
+			NSLog(@"on Mondays I would study under %d hours a day.", hrsMonDays);
+		} else if ((hrsMonDays >= 10) && (passedClass == true)) {
+			NSLog(@"on Mondays I would study %d hours a day and passed all my classes.", hrsMonDays);
+		} else {
+			NSLog(@"on Mondays I would not study.");
+		}
+		//Tuesdays Conditionals
+		if (hrsTuesDays < 10) {
+			NSLog(@"on Tuesdays I would study under %d hours a day.", hrsTuesDays);
+		} else if ((hrsTuesDays >= 10) || (passedClass == true)) {
+			NSLog(@"on Tuesdays I would study %d hours a day and passed all my classes.", hrsTuesDays);
+		} else {
+			NSLog(@"on Tuesdays I would not study.");
+		}
+		//Wednesdays Conditionals
+		if (hrsWedsDays < 10) {
+			NSLog(@"on Wednesdays I would study under %d hours a day.", hrsWedsDays);
+		} else if ((hrsWedsDays >= 10) && (passedClass == true)) {
+			NSLog(@"on Wednesdays I would study %d hours a day and passed all my classes.", hrsWedsDays);
+		} else {
+			NSLog(@"on Wednesdays I would not study.");
+		}
+		//Thursdays Conditionals
+		if (hrsThurDays < 10) {
+			NSLog(@"on Thursdays I would study under %d hours a day.", hrsThurDays);
+		} else if ((hrsThurDays >= 10) && (passedClass == true)) {
+			NSLog(@"on Thursdays I would study %d hours a day and passed all my classes.", hrsThurDays);
+		} else {
+			NSLog(@"on Thursdays I would not study.");
+		}
+		//Fridays Conditionals
+		if (hrsFriDays < 10) {
+			NSLog(@"on Fridays I would study under %d hours a day.", hrsFriDays);
+		} else if ((hrsFriDays >= 10) && (passedClass == true)) {
+			NSLog(@"on Fridays I would study %d hours a day and passed all my classes.", hrsFriDays);
+		} else {
+			NSLog(@"on Fridays I would not study.");
+		}
+		//Saturdays Conditionals
+		if (hrsSatDays < 10) {
+			NSLog(@"on Saturdays I would study under %d hours a day.", hrsSatDays);
+		} else if ((hrsSatDays >= 10) && (passedClass == true)) {
+			NSLog(@"on Saturdays I would study %d hours a day and passed all my classes.", hrsSatDays);
+		} else {
+			NSLog(@"on Saturdays I would not study.");
+		}
+		//Sundays Conditionals
+		if ((int)hrsSunDays < 10) {
+			NSLog(@"on Sundays I would study under %d hours a day.", (int)hrsSunDays);
+		} else if (((int)hrsSunDays >= 10) && (passedClass == true)) {
+			NSLog(@"on Sundays I would study %d hours a day and passed all my classes.", (int)hrsSunDays);
+		} else {
+			NSLog(@"on Sundays I would not study.");
+		}
 	
     return YES;
 }
